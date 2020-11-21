92e3b92958e7ea427513a0faa77b9b226e13fd4f
rojas70 <rojas70@gmail.com>
2020-11-21 14:09:41 +0800

    modifications to suit Dr. Juan Rojas class

--- setup_workspace_learning_ros_noetic.sh
+++ setup_workspace_learning_ros_noetic.sh
@@ -11,46 +11,43 @@ EMAIL=$2
 
 if [ "$USERNAME" != "" ] || [ "$EMAIL" != "" ];
 then
-  source /opt/ros/melodic/setup.bash
-  echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
+  source /opt/ros/noetic/setup.bash
+#  echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
   rosdep update
 
-  mkdir -p ~/ros_ws/src
+  ROS_BASE=~/ros/noetic/ros_ws
+  mkdir -p $ROS_BASE/src
 	
-  cd ~/ros_ws/src  && catkin_init_workspace
-  cd ~/ros_ws && catkin_make
+  cd $ROS_BASE/src  && catkin_init_workspace
+  cd $ROS_BASE && catkin_make
 
   git config --global user.name "$USERNAME"
   git config --global user.email "$EMAIL"
 
-  #https://github.com/wsnewman/learning_ros_kinetic.git
-  #https://github.com/wsnewman/learning_ros_external_packages_kinetic.git
-  #cd ~/ros_ws/src && git clone https://github.com/wsnewman/learning_ros_kinetic.git
-  #cd ~/ros_ws/src && git clone https://github.com/wsnewman/learning_ros_external_pkgs_kinetic.git
-  
-  # Cloning STDR is only necessary because of a bug in the current ROS binary release
-  # This prevents spawning a new robot on the screen. See this bug report for more information:
+  # Cloning STDR is necessary for noetic and bug in the current ROS binary release
   # https://github.com/stdr-simulator-ros-pkg/stdr_simulator/issues/195
 
-  # cd ~/ros_ws/src && git clone https://github.com/stdr-simulator-ros-pkg/stdr_simulator.git
+  cd $ROS_BASE/src
+  git clone https://github.com/rojas70/stdr_simulator.git
 
-  echo "source ~/ros_ws/devel/setup.bash" >> ~/.bashrc
+  # Change to the noetic-devel branch
+  git checkout noetic-devel
+
+  echo "source ~/ros/noetic/ros_ws/devel/setup.bash" >> ~/.bashrc
   source ~/.bashrc
 
-  cd ~/ros_ws && catkin_make
-  cd ~/ros_ws && catkin_make install
+  cd ~/$ROS_BASE && catkin_make
+  cd ~/$ROS_BASE && catkin_make install
 
 
-  echo "alias cs_create_pkg='~/ros_ws/src/learning_ros_external_pkgs_kinetic/cs_create_pkg.py'" >> ~/.bashrc
+  echo "alias cs_create_pkg='~/ros/noetic/ros_ws/src/learning_ros_external_pkgs_noetic/cs_create_pkg.py'" >> ~/.bashrc
   echo "source ~/ros_ws/devel/setup.bash" >> ~/.bashrc
   echo "export ROS_WORKSPACE='$HOME/ros_ws'" >> ~/.bashrc
   # echo "export ROS_IP=`ifconfig eth0 2>/dev/null|awk '/inet addr:/ {print $2}'|sed 's/addr://'`" >> ~/.bashrc
-
-
 
 else
   echo "USAGE: ./setup_workspace_learning_ros your_github_username your_email@email.com"
 
 fi
 
-echo "[!!!] NB: You must still manually add your ROS_IP to your ~/.bashrc.  Do this by checking your IP with hostname -I or ifconfig and then adding export ROS_IP='x.x.x.x' to your ~/.bashrc."
+#echo "[!!!] NB: You must still manually add your ROS_IP to your ~/.bashrc.  Do this by checking your IP with hostname -I or ifconfig and then adding export ROS_IP='x.x.x.x' to your ~/.bashrc."
